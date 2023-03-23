a57ba2de1afacb8bbbfb1c1678c6e036835b74cd
davidbreau <davidbreau@live.fr>
2023-03-23 14:45:07 +0100

    Serveur local ok

---
+++ suggestion_box/core/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    birth_date = models.DateField(auto_now=False, null=True)
